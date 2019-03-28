2ab4440a8cf7b9de99f7dd71004e269a1ec1bf9b
aomkirby123 <aomkirby123@gmail.com>
2019-03-29 06:59:29 +0700

    Fixed Column bug

--- src/index.js
+++ src/index.js
@@ -42,16 +42,23 @@ class App extends React.Component {
             <>
                 <div id="background"></div>
                 <div id="scoreboard">
-                    <div className="card-wrapper">
-                        {this.state.data.map((data,index) => 
+                    {this.state.data.map((data,index) => 
+                        {index % 4 === 0 ?
+                            <div className="card-wrapper">
                                 <div className="card" key={index}>
                                     <h1>{data.point}</h1>
                                     <h6>Gemstones</h6>
                                     <p>{data.name}</p>
                                 </div>
-                            )
+                            </div>
+                            :
+                            <div className="card" key={index}>
+                                <h1>{data.point}</h1>
+                                <h6>Gemstones</h6>
+                                <p>{data.name}</p>
+                            </div>
                         }
-                    </div>
+                    )}
                 </div>
             </>
         )
